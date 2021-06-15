01dd57749b817492a65269b1db54c3d462cce000
abhi887 <vyasa887@gmail.com>
2021-06-16 03:59:19 +0530

    added: message seperation string

--- app.py
+++ app.py
@@ -18,13 +18,10 @@ def index():
 @app.route('/logs',methods=["GET","POST"])
 def logs():
     LOGS = request.data.decode("utf-8")
-    print(f"LOGS = {LOGS}")
-    # logs = '''\
-    #     83 <40>1 2012-11-30T06:45:29+00:00 host app web.3 - State changed from starting to up
-    #     119 <40>1 2012-11-30T06:45:26+00:00 host app web.3 - Starting process with command `bundle exec rackup config.ru -p 24405`'''
     for i in LOGS.split('\n'):
         try:
             send_discord_message(re.split(">[a-zA-Z0-9]* ",i.strip())[1])
+            send_discord_message("------".center(30))
         except IndexError:
             pass
     return jsonify({"status":"Logged !"})
